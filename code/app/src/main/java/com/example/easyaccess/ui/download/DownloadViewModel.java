ebbed75a573590cbb9240175e8b8b1253772bae8
jiengupxing <jiengup@gamil.com>
2020-05-06 14:12:09 +0800

    first commit resourse code

---
+++ code/app/src/main/java/com/example/easyaccess/ui/download/DownloadViewModel.java
@@ -0,0 +1,19 @@
+package com.example.easyaccess.ui.download;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DownloadViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DownloadViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ code/app/src/main/java/com/example/easyaccess/ui/team/TeamViewModel.java
@@ -0,0 +1,19 @@
+package com.example.easyaccess.ui.team;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class TeamViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public TeamViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
